886842563eaf8b71347890506fc2801bed122c91
Sarah Floyd <sarahmfloyd21@gmail.com>
2025-09-30 10:16:41 -0500

    switch page content into layout instead

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,8 @@ import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <>
-      <header>
-        <h1>Header</h1>
-      </header>
-      <main>
-        <h1>Main Content</h1>
-      </main>
-      <footer>
-        <p>Footer</p>
-      </footer>
-    </>
+    <main>
+      <h1>Main Content</h1>
+    </main>
   );
 }
